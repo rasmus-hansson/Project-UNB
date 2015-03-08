a004fb675faf0c02a5975fc40180ff8264b0c4f9
Rasmus Hansson <hej@rasmushansson.com>
2015-03-08 22:56:31 +0100

    Simplified code by removing one switch statement

--- script.js
+++ script.js
@@ -22,55 +22,13 @@ function setColor() {   //function to set color and position, called by var star
 
 var index;
 var gamesquare = [gs1, gs2, gs3, gs4, gs5, gs6, gs7, gs8, gs9];
-  for (index = 0; index < gamesquare.length; index++) {
+for (index = 0; index < gamesquare.length; index++) {
     gamesquare[index].style.backgroundColor = "white"; };  // resets square before start of loop
 
 var rand1 = Math.floor(Math.random() * 9);   // returns a random number between 0 and 8 for color
 var rand2 = Math.floor(Math.random() * 9);   // returns a random number between 0 and 8 for position
 
-switch (rand2) { 
-
-    case 0:
-      randpos = gs1;
-    break;
-
-    case 1:
-      randpos = gs2;
-    break;
-
-    case 2:
-      randpos = gs3;
-    break;
-
-    case 3:
-      randpos = gs4;
-    break;
-
-    case 4:
-      randpos = gs5;
-    break;
-
-    case 5:
-      randpos = gs6;
-    break;
-
-    case 6:
-      randpos = gs7;
-    break;
-
-    case 7:
-      randpos = gs8;
-    break;
-    
-    case 8:
-      randpos = gs9;
-    break;    
-
-    default:
-
-  }
-
-  switch (rand1) {   // I tried a switch-statement for setting color & position instead of if-statements, but couldn't get it to work
+switch (rand1) {   // I tried a switch-statement for setting color & position instead of if-statements, but couldn't get it to work
 
     case 0:
       randcol = "red";
@@ -115,14 +73,18 @@ switch (rand2) {
     default:
   }
 
-  console.log('Position ' + randpos.id + ' Color ' + randcol);
+console.log('Position ' + gamesquare[rand2].id + ' Color ' + randcol);
 
-  randpos.style.backgroundColor = randcol;  // sets color and position for square
+  gamesquare[rand2].style.backgroundColor = randcol;  // sets color and position for square
   x++;                                      // increments x on each loop iteration
 
   if(x > rounds){
     clearInterval(start); // stops engine after x is above rounds
   }; 
+
+  
+
+  
 
 };
 
