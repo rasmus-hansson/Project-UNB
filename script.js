3d84bd8d4f16eb9a3d129eba627b521e7cd09121
Rasmus Hansson <hej@rasmushansson.com>
2015-03-10 22:33:35 +0100

    Connected level engine with game loop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('click', clickTrack, false); // Capture all click events with event listener
 
 
-//var start = setInterval(function(){ setColor() }, 2000); // starts engine, runs it every 2 seconds
+var start = setInterval(function(){ setColor() }, 2000); // starts engine, runs it every 2 seconds
 
 var x = 0; // Keep track of which round we're at
 var rounds = 50;
@@ -9,7 +9,7 @@ var matches = 20;
 var back = 3;
 var levelArr = [];
 var colors = ['red','green','orange','yellow'];
-var board = 9;
+var board = 8;
 
 // The idea of this section is to improve the level generator to include
 // color matching, position matching and thirdly a match of all Stimuli 
@@ -82,7 +82,7 @@ for (i=0 ; i<totalPosMatches ; i++) {
   if (levelArr[rndMatch] == null && levelArr[rndMatch-back] == null ) {
 
     // Generate random matching values
-    var rndCol = Math.floor(Math.random() * colors.length);    
+    var rndCol = Math.floor(Math.random() * colors.length);
     var rndPos = Math.floor(Math.random() * board + 1);
     var secCol = 0;
 
@@ -124,7 +124,7 @@ for (i=0 ; i<totalColMatches ; i++) {
     var rndPos = Math.floor(Math.random() * board + 1);
     var secPos = 0;
 
-    // This is currently a hack. If the algorithm works overall, we need to 
+    // This is currently a hack. We need to 
     // get a proper random number that doesn't match the first random number
     if (rndPos > 4) secPos = rndPos - 2;
     else secCol = rndPos + 2;
@@ -193,28 +193,26 @@ function setColor() {   //function to set color and position, called by var star
   var index;
   var gamesquare = [gs1, gs2, gs3, gs4, gs5, gs6, gs7, gs8, gs9];
   for (index = 0; index < gamesquare.length; index++) {
-      gamesquare[index].style.backgroundColor = "white"; };  // resets square before start of loop
+      gamesquare[index].style.backgroundColor = "white"; };  // resets square before start of loop  
 
-  var rand2 = Math.floor(Math.random() * 9);   // returns a random number between 0 and 8 for position
-
-  console.log('Position ' + gamesquare[rand2].id + ' Color ' + colors[levelArr[x]]);
-
-  gamesquare[rand2].style.backgroundColor = colors[levelArr[x]];  // sets color and position for square
+  gamesquare[levelArr[x][1]].style.backgroundColor = colors[levelArr[x][0]];  // sets color and position for square
   x++;                                      // increments x on each loop iteration
 
   if(x > rounds){
     clearInterval(start); // stops engine after x is above rounds
-  }; 
+  };  
 
-  // Callback routine for RequestAnimationFrame
-  function gameLoop() {
-    // My idea here is to check the time value and when 2 seconds 
-    // has passed we update the time variable and also update the
-    // game board at the same time.
-    //
-    //
-  };
+};
 
+
+
+// Callback routine for RequestAnimationFrame
+function gameLoop() {
+  // My idea here is to check the time value and when 2 seconds 
+  // has passed we update the time variable and also update the
+  // game board at the same time.
+  //
+  //
 };
 
 
