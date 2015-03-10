9f8bdfe01174b5ee3cba5ffe4b9753c27e513c69
Rasmus Hansson <hej@rasmushansson.com>
2015-03-10 22:01:45 +0100

    Rebuilt the level engine

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var colors = ['red','green','orange','yellow'];
 var board = 9;
 
 // The idea of this section is to improve the level generator to include
-// color matching, position matching and a match of all Stimuli 
+// color matching, position matching and thirdly a match of all Stimuli 
 // (in this case a combination of color and position)
 // If we decide to use it, we will need to replace the
 // above matches var and modify the routine below accordingly
@@ -22,83 +22,153 @@ var colMatches = 0.3;
 var posMatches = 0.4;
 var allMatches = 0.2;
 
-var totalPosMatches = Math.round(rounds * posMatches);
-var totalColMatches = Math.round(rounds * colMatches);
-var totalAllMatches = Math.round(rounds * allMatches);
+var totalPosMatches = Math.round(rounds * posMatches) / 2;
+var totalColMatches = Math.round(rounds * colMatches) / 2;
+var totalAllMatches = Math.round(rounds * allMatches) / 2;
 var totalMatches = Math.round(rounds * (colMatches + posMatches + allMatches));
   
   console.log ('Color matches ' + totalColMatches);
   console.log ('Position matches ' + totalPosMatches);
   console.log ('All matches ' + totalAllMatches);
   console.log ('Total matches ' + totalMatches);
-//
-//
-////////////////////////////////////////////////////////////////////////////////
-
-
-// This routine attempts to build a level with params from above.
-// Todo: Check that number of matches is actually what we say
-// Todo: It would be nice to see or track how many (extra) iterations were necessary to generate the level
-// Todo: It seems that the routine sometimes get stuck which probably means there is no possibility to generate
-// the filler value becuase of how the matches are composed. If so we will probably have to rewrite the level 
-// where it gets stuck...
-// Todo: Test edge cases and attempt to break (for example, what to do when adding more or equal matches to rounds)
-// 
-//
-var i = 0;
 
 // First create an array full of nulls
 for (i=0 ; i<rounds ; i++) {
   levelArr[i] = null;
 }
 
-// Next, create all the matches
-for (i=0 ; i<matches ; i++) {
+var iterationCounter = 0;
 
-  var templevelArr = Math.floor(Math.random() * (rounds - back)) + back; // Random position between back and total rounds
-  if ( levelArr[templevelArr] == null && levelArr[templevelArr-back] == null ) {
-    tempValue = Math.floor(Math.random() * colors.length );
-    
-    // Create the match
-    levelArr[templevelArr] = tempValue;
-    levelArr[templevelArr-back] = tempValue; 
+// Next iterate over the amount of allMatches
+for (i=0 ; i<totalAllMatches ; i++) {
+  
+  // Random position for a match
+  var rndMatch = Math.floor(Math.random() * (rounds - back)) + back;  
 
-  } else {
+  // If this random position is valid, insert the match
+  if ( levelArr[rndMatch] == null && levelArr[rndMatch-back] == null ) {
 
-    i--; // Don't count this iteration if we didn't find a possible match position (this is wasteful stuff tho)
+    // Generate random matching values
+    var rndCol = Math.floor(Math.random() * colors.length);
+    var rndPos = Math.floor(Math.random() * board + 1);
 
+    // Insert both color and position at match location and match-back location
+    levelArr[rndMatch] = [rndCol, rndPos];
+    levelArr[rndMatch-back] = [rndCol, rndPos];
+
+  } else { 
+    i--;
   }
 
-  // Create some output in the console for debugging purposes and fun
-  console.log (levelArr.join(', '));  
+  // Increment iteration counter to keep track of efficiency
+  iterationCounter ++;
+
+  // Print out the current level array for debugging purpose
+  console.log (levelArr.join(', '));
 
 }
 
-// Last, iterate the level and create all the non-matches as "levelfillers"
-for (i=0 ; i<rounds ; i++) {
-  if ( levelArr[i] == null) { // only work with the empty 0 positions
-    tempValue = Math.floor(Math.random() * colors.length );
-    if ( tempValue != levelArr[i-back] && tempValue != levelArr[i+back] ) {
-      
-      // Create the filler
-      levelArr[i] = tempValue;
+console.log ('Total iterations ' + iterationCounter);
+//console.log ('Iterations wasted ' + (iterationCounter-totalAllMatches));
 
-    } else {
 
-      i--; // Don't count this iteration if we didn't find a possible match position (this is wasteful stuff tho)
 
+// Next iterate over the amount of allPositions
+for (i=0 ; i<totalPosMatches ; i++) {
+
+  // Try to find random position for a match
+  var rndMatch = Math.floor(Math.random() * (rounds - back)) + back;
+
+  if (levelArr[rndMatch] == null && levelArr[rndMatch-back] == null ) {
+
+    // Generate random matching values
+    var rndCol = Math.floor(Math.random() * colors.length);    
+    var rndPos = Math.floor(Math.random() * board + 1);
+    var secCol = 0;
+
+    // This is currently a hack. If the algorithm works overall, we need to 
+    // get a proper random number that doesn't match the first random number
+    if (rndCol > 5) secCol = rndCol - 2;
+    else secCol = rndCol + 2;
+
+    // Insert both color and position at match location and match-back location
+    levelArr[rndMatch] = [rndCol, rndPos];
+    levelArr[rndMatch-back] = [secCol, rndPos];
+
+  } else { 
+    i--; 
+  }
+
+  // Print out the current level array for debugging purpose
+  console.log (levelArr.join(', '));
+
+  // Increment iteration counter to keep track of efficiency
+  iterationCounter ++;
+
+}
+
+console.log ('Total iterations ' + iterationCounter);
+//console.log ('Iterations wasted ' + (iterationCounter-totalAllMatches));
+
+
+// Next iterate over the amount of allColors
+for (i=0 ; i<totalColMatches ; i++) {
+
+  // Try to find random position for a match
+  var rndMatch = Math.floor(Math.random() * (rounds - back)) + back;
+
+  if (levelArr[rndMatch] == null && levelArr[rndMatch-back] == null ) {
+
+    // Generate random matching values
+    var rndCol = Math.floor(Math.random() * colors.length);    
+    var rndPos = Math.floor(Math.random() * board + 1);
+    var secPos = 0;
+
+    // This is currently a hack. If the algorithm works overall, we need to 
+    // get a proper random number that doesn't match the first random number
+    if (rndPos > 4) secPos = rndPos - 2;
+    else secCol = rndPos + 2;
+
+    // Insert both color and position at match location and match-back location
+    levelArr[rndMatch] = [rndCol, rndPos];
+    levelArr[rndMatch-back] = [rndCol, secPos];
+
+  } else { 
+    i--; 
+  }
+
+  // Print out the current level array for debugging purpose
+  console.log (levelArr.join(', '));
+
+  // Increment iteration counter to keep track of efficiency
+  iterationCounter ++;
+
+  if (iterationCounter > 1000) {
+    console.log ('Assuming no more matches, aborting...');
+    break;
+  }
+}
+
+console.log ('Total iterations ' + iterationCounter);
+//console.log ('Iterations wasted ' + (iterationCounter-totalAllMatches));
+
+
+// Now we want to create non-matching values for the rest of the level array
+// We do this by looping the level array
+for (i=0 ; i<levelArr.length ; i++) {
+    if (levelArr[i] == null) {
+      if (i < back) { // If the empty slot is in the beginning we need to make sure there isn't a match forward (todo currently unsafe hack)
+        var tmpValues = levelArr[i+back];
+        levelArr[i] = [tmpValues[0]+1,tmpValues[1]+1];
+      } else { // If the empty slot is later we need to make sure there isn't a match forward and backward (todo, currntly unsafe hack)
+        var tmpValues = levelArr[i-back];
+        levelArr[i] = [tmpValues[0]+1, tmpValues[1]+1];
+      }
     }
-    // Create some output in the console for debugging purposes and fun
-    console.log (levelArr.join(', '));  
-  }
+
+    console.log (levelArr.join(', '));
 }
-
-// Create some output in the console for debugging purposes and fun
-  //console.log (levelArr.join(', '));
-  console.log (levelArr.length);
-
-//////////////////////// Level Routine End /////////////////////////
-
+  
 
 /////////////////
 // Click Tracking
