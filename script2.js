71ef5c7ebe635dd74c912d0d6755491df4e78e0f
Rasmus Hansson <hej@rasmushansson.com>
2015-03-14 19:29:24 +0100

    Made it possible to start and restart game (with bugs)

--- script2.js
+++ script2.js
@@ -8,7 +8,7 @@
 // Monte carlo simulation
 // graphics
 
-document.addEventListener('click', clickTrack, false); // Capture all click events with event listener
+var start;
 
 var stimColor = ['Purple','Green','Orange','Yellow', 'Red', 'Pink','Aqua'];
 //var stimPosition = [gs1, gs2, gs3, gs4, gs5, gs6, gs7, gs8, gs9];
@@ -21,112 +21,119 @@ var wrongArr = []; //contains number of wrong clicks
 var playedGames = [];
 var gameSquares = 9;
 
-var stimPositionSetting = []
+var stimPositionSetting = [];
 
-var x = 0;  //What round we are on
 var back = 3; //Number of N-back
-var rounds = 20; //Number of rounds per game
+var rounds = 5; //Number of rounds per game
 
 var i = 0; //Iterator for gameloop
 var synthMatch = null; // counts number of synthetic matches
 
 //localStorage.removeItem('playedgames');
 
-// This routine is supposed to generate game squares based on stimposition length
-//
-var squareshtml = "";
-for (i=0 ; i < gameSquares ; i++) {
-  squareshtml += "<div class='gamesquare' id=\"" + 'gs'+i + "\"></div>"; 
-  stimPosition.push('gs'+i);
-  stimPositionSetting.push('gs'+stimPosition[i]);
-  if(i === 2 || i === 5 || i === 8) {
-    squareshtml += "<br>";
+var x;  //What round we are on
+
+function setupGame() {
+
+  x=0;  
+
+  // This routine is supposed to generate game squares based on stimposition length
+  //
+  var squareshtml = "";
+  for (i=0 ; i < gameSquares ; i++) {
+    squareshtml += "<div class='gamesquare' id=\"" + 'gs'+i + "\"></div>"; 
+    stimPosition.push('gs'+i);
+    stimPositionSetting.push('gs'+stimPosition[i]);
+    if(i === 2 || i === 5 || i === 8) {
+      squareshtml += "<br>";
+    }
   }
-}
-console.log(squareshtml);
+  console.log(squareshtml);
 
-  console.log('this many stimpositions'+stimPosition);
-gamesquares.innerHTML = squareshtml;
-//
+    console.log('this many stimpositions'+stimPosition);
+  gamesquares.innerHTML = squareshtml;
+  //
 
 
-for ( i=0 ; i<rounds ; i++ ) {       //Loop that enables percent matches per game to equal matchPercent, and 
-  console.log(i);                    //generates random stimuli in the other cases, then pushes those positions to levelArray
-  var rnd = Math.random();
-  if ((rnd < matchPercent) && (i >= (back))) {    // ATT GÖRA: LÄGG TILL ELSE IFS FÖR ATT GENERERA FORCED MATCHING AV BARA COLOR ELLER POSITION
-  
-    levelArray[i] = levelArray[i-back];
-    matchArr.push(1);
-    console.log(levelArray[i-back]);
-    synthMatch++;
+  for ( i=0 ; i<rounds ; i++ ) {       //Loop that enables percent matches per game to equal matchPercent, and 
+    console.log(i);                    //generates random stimuli in the other cases, then pushes those positions to levelArray
+    var rnd = Math.random();
+    if ((rnd < matchPercent) && (i >= (back))) {    // ATT GÖRA: LÄGG TILL ELSE IFS FÖR ATT GENERERA FORCED MATCHING AV BARA COLOR ELLER POSITION
+    
+      levelArray[i] = levelArray[i-back];
+      matchArr.push(1);
+      console.log(levelArray[i-back]);
+      synthMatch++;
 
-  } else {        // ATT GÖRA: LÄGG IN TRACKING AV NATURLIGA MATCHES SOM PUSHAS TILL matchArr!!!!
-    var rndColor = Math.floor(Math.random() * ( stimColor.length));
-    var rndPosition = Math.floor(Math.random() * ( stimPosition.length));
+    } else {        // ATT GÖRA: LÄGG IN TRACKING AV NATURLIGA MATCHES SOM PUSHAS TILL matchArr!!!!
+      var rndColor = Math.floor(Math.random() * ( stimColor.length));
+      var rndPosition = Math.floor(Math.random() * ( stimPosition.length));
 
-    levelArray.push([rndPosition, rndColor]);
-    //console.log(levelArray[i-back]);
-      
+      levelArray.push([rndPosition, rndColor]);
+      //console.log(levelArray[i-back]);
+        
 
-      if((i >= (back)) && (rndColor === levelArray[i-back][1]) ){
+        if((i >= (back)) && (rndColor === levelArray[i-back][1]) ){
 
-        matchArr.push(2);
+          matchArr.push(2);
 
-        console.log('A natural match!');
-      }
-      else if((i >= (back)) && (rndPosition === levelArray[i-back][0])){
+          console.log('A natural match!');
+        }
+        else if((i >= (back)) && (rndPosition === levelArray[i-back][0])){
 
-        matchArr.push(3);
+          matchArr.push(3);
 
-        console.log('A natural match!');
-      }
+          console.log('A natural match!');
+        }
 
-        else if((i >= (back)) && (levelArray[i] ===  levelArray[i-back])){
+          else if((i >= (back)) && (levelArray[i] ===  levelArray[i-back])){
 
-          matchArr.push(4);
+            matchArr.push(4);
 
-        console.log('A natural match!');
-        };
+          console.log('A natural match!');
+          };
+
+    };
 
   };
 
-};
-
-console.log (levelArray.join(', '));
+  console.log (levelArray.join(', '));
 
 
-console.log (matchArr); //logs contents of matcharray
-console.log (matchArr.length + ' matches in this round. Whereof '+synthMatch+' are synthetic, and '+(matchArr.length-synthMatch)+' are natural!'); //logs number of synthetic matches
+  console.log (matchArr); //logs contents of matcharray
+  console.log (matchArr.length + ' matches in this round. Whereof '+synthMatch+' are synthetic, and '+(matchArr.length-synthMatch)+' are natural!'); //logs number of synthetic matches
 
-// Handle the display of latest score
-//
-//
-var playedGames = JSON.parse(localStorage.getItem('playedgames')); // Try to get local storage item
+  // Handle the display of latest score
+  //
+  //
+  var playedGames = JSON.parse(localStorage.getItem('playedgames')); // Try to get local storage item
 
-if (playedGames) {     // This conditional will display all your saved results in the gamescreen
+  if (playedGames) {     // This conditional will display all your saved results in the gamescreen
 
-  console.log (playedGames);
+    console.log (playedGames);
 
-  game = "<table>";
+    game = "<table>";
 
-  for (i=0 ; i < playedGames.length; i++) {
-    game += "<tr>";
-    game += "<td>" + i + "</td>";
-    game += "<td>" + playedGames[i].time + "</td>";
-    game += "<td>" + playedGames[i].scorepercent + "</td>";
-    game += "<td>" + playedGames[i].config.nback + "</td>";
-    game += "<td>" + playedGames[i].config.matchpercent + "</td>";
-    game += "</tr>";
+    for (i=0 ; i < playedGames.length; i++) {
+      game += "<tr>";
+      game += "<td>" + i + "</td>";
+      game += "<td>" + playedGames[i].time + "</td>";
+      game += "<td>" + playedGames[i].scorepercent + "</td>";
+      game += "<td>" + playedGames[i].config.nback + "</td>";
+      game += "<td>" + playedGames[i].config.matchpercent + "</td>";
+      game += "</tr>";
+    }
+    game += "</table>";
+    console.log(game);
+    result.innerHTML = game;
+  } else {
+    playedGames = [];
   }
-  game += "</table>";
-  console.log(game);
-  result.innerHTML = game;
-} else {
-  playedGames = [];
 }
 
-
-
+setupGame();
+start = setInterval(function(){ setColor() }, 2000); // starts engine, runs it every 2 seconds
+document.addEventListener('click', clickTrack, false); // Capture all click events with event listener
 
 function setColor() {   //function to set color and position based on what's in levelArray, called by var start
 
@@ -138,7 +145,7 @@ function setColor() {   //function to set color and position based on what's in 
       
       document.getElementById(stimPosition[z]).style.backgroundColor = "white"; 
       document.getElementById(stimPosition[z]).style.transitionDuration = "2.5s";  // resets square before start of loop 
-}
+    }
 
     //console.log ('Round count ' + (x+1));
     //console.log ('Position ' + (levelArr[x][1]+1) + ' Color ' + colors[levelArr[x][0]]);
@@ -150,31 +157,30 @@ function setColor() {   //function to set color and position based on what's in 
       document.getElementById(stimPosition[levelArray[x][0]]).style.transitionDuration = "0.1s"; 
             };
                              
-  }
-  else{
-    console.log('Level complete, stopping engine');
-    console.log(answerArr);
-    clearInterval(start); // stops engine after x is above rounds
-    console.log('Your scored '+answerArr.length+' right, out of '+matchArr.length+' possible!\n You had '+wrongArr.length+' incorrect answers.');  //logs number of correct answers
-      document.getElementById("result").innerHTML = 'Your scored '+answerArr.length+' right, out of '+matchArr.length+' possible! <br> You had '+wrongArr.length+' incorrect answers.';
-      
-      var game = {
-        time: Date(),
-        scorepercent: answerArr.length,
-        config: {
-          nback: back,
-          matchpercent: matchPercent
-        }
-      };     
+    }else{
+      console.log('Level complete, stopping engine');
+      console.log(answerArr);
+      console.log("interval "+ start);
+      clearInterval(start); // stops engine after x is above rounds
+      console.log('Your scored '+answerArr.length+' right, out of '+matchArr.length+' possible!\n You had '+wrongArr.length+' incorrect answers.');  //logs number of correct answers
+        document.getElementById("result").innerHTML = 'Your scored '+answerArr.length+' right, out of '+matchArr.length+' possible! <br> You had '+wrongArr.length+' incorrect answers.';
+        
+        var game = {
+          time: Date(),
+          scorepercent: answerArr.length,
+          config: {
+            nback: back,
+            matchpercent: matchPercent
+          }
+        };     
 
-      // Save this game in array of games for local storage
-      playedGames.push(game);
+        // Save this game in array of games for local storage
+        playedGames.push(game);
 
-      //console.log(playedGames);
-      localStorage.setItem('playedgames', JSON.stringify(playedGames));  //stores number of correct answers in local file
-  }
-      x++;  // increments x on each run of the function setColor, called by setInterval in var start
-          
+        //console.log(playedGames);
+        localStorage.setItem('playedgames', JSON.stringify(playedGames));  //stores number of correct answers in local file
+    }
+  x++;  // increments x on each run of the function setColor, called by setInterval in var start          
 };
 
 
@@ -231,12 +237,16 @@ function clickTrack (ev) {
       i++;
 
     break;
-
-     case "play":
-      var start = setInterval(function(){ setColor() }, 2000); // starts engine, runs it every 2 seconds
+    case "play":
+      //var start = setInterval(function(){ setColor() }, 2000); // starts engine, runs it every 2 seconds
     break;
     case "restart":
-      restartGame();
+      console.log(JSON.stringify(start));
+      clearInterval(start); // stops engine after x is above rounds
+      setupGame();  
+      start = setInterval(function(){ setColor() }, 2000); // starts engine, runs it every 2 seconds
+      
+
     break;
   }
 };
